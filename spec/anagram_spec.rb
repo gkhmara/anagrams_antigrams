5b8e2990927df40739e9703532f7eeefc9a73a65
Greg Khmara <gkhmara@gmail.com>
2022-04-23 13:05:10 -0700

    create second test for cases where inputs are not anagrams.

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,14 +4,15 @@ require('anagram_logic')
 describe Anagrams_Antigrams do
   describe '#read_anagram' do
 
-    # it("should take in two inputs and check to see if they are anagrams") do
-    #   user_input = Anagrams_Antigrams.new
-    #   expect(user_input.read_anagram("greg")).to(eq("greg"))
-    # end
-
     it("should take in two inputs and check to see if they are anagrams") do
       user_input = Anagrams_Antigrams.new
       expect(user_input.read_anagram("greg", "greg")).to(eq("These words are anagrams."))
     end
+
+    it("should take in two input and check to see if they are anagrams") do
+      user_input = Anagrams_Antigrams.new
+      expect(user_input.read_anagram("hat", "bat")).to(eq("These words are not anagrams."))
+    end
+
   end
 end
