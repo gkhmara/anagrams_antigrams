e608783d35bb0358f352fc9fce021f51faf2139e
Greg Khmara <gkhmara@gmail.com>
2022-04-30 20:30:08 -0700

    update script for user input and output

--- anagram_script.rb
+++ anagram_script.rb
@@ -2,6 +2,21 @@
 
 require ('./lib/anagram_logic.rb')
 
-user_input = Anagrams_Antigrams.new
+puts 'Please enter two words or sentences to check if they are anagrams or antigrams'
+word1 = gets.chomp
+word2 = gets.chomp
+user_input = Anagrams_Antigrams.new(word1, word2)
 
-puts user_input.read_anagram(user_input.input1, user_input.input2)
+if user_input.is_word?
+  if user_input.is_antigram?
+    if user_input.is_anagram?
+      puts "These inputs are anagrams"
+    else
+      puts "These inputs are not anagrams"
+    end
+  else
+    puts "These inputs have no letter matches and are antigrams"
+  end
+else
+  puts "One of your inputs is not a word. Please try again."
+end
