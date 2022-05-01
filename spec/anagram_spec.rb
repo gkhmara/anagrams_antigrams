240c4b043c2073590d1ee312246abdf82e06d48d
Greg Khmara <gkhmara@gmail.com>
2022-04-30 20:29:18 -0700

    update tests to reflect new methods

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -2,32 +2,41 @@ require('rspec')
 require('anagram_logic')
 
 describe Anagrams_Antigrams do
-  describe '#read_anagram' do
 
-    it("should take in two inputs and check to see if they are anagrams") do
-      user_input = Anagrams_Antigrams.new
-      expect(user_input.read_anagram("greg", "greg")).to(eq("These inputs are anagrams."))
-    end
+  describe '#is_word?' do
 
-    it("should take in two input and check to see if they are anagrams") do
-      user_input = Anagrams_Antigrams.new
-      expect(user_input.read_anagram("hat", "bat")).to(eq("These inputs are not anagrams."))
-    end
-
-    it("should return an error if one of the inputs doesn't contain a vowel") do
-      user_input = Anagrams_Antigrams.new
-      expect(user_input.read_anagram("thd", "plmn")).to(eq("One of your inputs is not a word. Please try again."))
-    end
-
-    it("should return an error if both inputs have no matching letters") do
-      user_input = Anagrams_Antigrams.new
-      expect(user_input.read_anagram("greg", "hat")).to(eq("These inputs have no letter matches and are antigrams."))
-    end
-
-    it("should test to see if two phrases are anagrams regardless of spaces or puncuation.") do
-      user_input = Anagrams_Antigrams.new
-      expect(user_input.read_anagram("The Morse Code", "Here come dots!")).to(eq("These inputs are anagrams."))
+    it("should take in two inputs and check to see if they are words") do
+      user_input = Anagrams_Antigrams.new("rnt", "fbt")
+      expect(user_input.is_word?).to(eq(false))
     end
 
   end
+
+  describe '#is_anagram?' do
+
+    it("should take in two inputs and check to see if they are anagrams") do
+      user_input = Anagrams_Antigrams.new("greg", "gerg")
+      expect(user_input.is_anagram?).to(eq(true))
+    end
+
+    it("should take in two inputs and check to see if they are not anagrams") do
+      user_input = Anagrams_Antigrams.new("hat", "bat")
+      expect(user_input.is_anagram?).to(eq(false))
+    end
+
+    it("should test to see if two phrases are anagrams regardless of spaces or punctuation") do
+      user_input = Anagrams_Antigrams.new("The Morse Code", "Here come dots!")
+      expect(user_input.is_anagram?).to(eq(true))
+    end
+  
+  end
+
+  describe '#is_antigram?' do
+    it("should take in two inputs and check to see if they are antigrams") do
+      user_input = Anagrams_Antigrams.new("cat", "dog")
+      expect(user_input.is_antigram?).to(eq(true))
+    end
+
+  end
+
 end
